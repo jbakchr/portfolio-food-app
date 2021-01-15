0c40f550d7abcb60ffe78586d988dfc368a0e6ae
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-01-15 13:50:56 +0100

    Added material-ui and initial navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>Navbar here</p>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import Navbar from "../components/Navbar";
+
 const LandingPage = () => {
   return (
     <div>
+      <Navbar />
       <p>Landing Page</p>
     </div>
   );
