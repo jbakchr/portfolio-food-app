121d1193af1f6b864e13a3d316d9b03c2884eb5a
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-01-15 09:32:26 +0100

    Added initial local db setup

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,12 @@
 const Sequelize = require("sequelize");
 
-console.log(process.env);
+const DB_PASSWORD = process.env.MYSQL_ROOT_PASSWORD
+  ? "Esmun"
+  : process.env.DB_PASSWORD;
+const HOST = process.env.DB_HOST ? "localhost" : "db";
 
-const db = new Sequelize("foodapp", "root", "Esmun", {
-  host: "db",
+const db = new Sequelize("foodapp", "root", DB_PASSWORD, {
+  host: HOST,
   dialect: "mysql",
 });
 
