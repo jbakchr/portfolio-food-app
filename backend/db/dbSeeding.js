ed2fd1cf49e2d4d387ae8fabbbe887645d7ff526
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-01-17 15:24:18 +0100

    Initial recipe data added

--- backend/db/dbSeeding.js
+++ backend/db/dbSeeding.js
@@ -1,10 +1,13 @@
 const Ingredient = require("../models/Ingredient");
+const Recipe = require("../models/Recipe");
 
 const ingredients = require("../data/ingredients.json");
+const recipes = require("../data/recipes.json");
 
 const dbSeeding = async () => {
   try {
     await Ingredient.bulkCreate(ingredients);
+    await Recipe.bulkCreate(recipes);
   } catch (error) {
     console.log(error);
   }
--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -2,10 +2,16 @@ const { DataTypes } = require("sequelize");
 
 const db = require("../db/db");
 
-const Recipe = db.define("recipe", {
-  recipe: DataTypes.STRING,
-  ingredients: DataTypes.TEXT,
-  recipe_text: DataTypes.TEXT,
-});
+const Recipe = db.define(
+  "recipe",
+  {
+    recipe: DataTypes.STRING,
+    ingredients: DataTypes.TEXT,
+    recipe_text: DataTypes.TEXT,
+  },
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Recipe;
