ff55e2c4298cc7a47e05ca16628e9c253ba7ddab
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-01-17 20:32:04 +0100

    Added initial ingredient_recipe data

--- backend/db/dbSeeding.js
+++ backend/db/dbSeeding.js
@@ -1,13 +1,17 @@
+const db = require("../db/db");
+
 const Ingredient = require("../models/Ingredient");
 const Recipe = require("../models/Recipe");
 
 const ingredients = require("../data/ingredients.json");
 const recipes = require("../data/recipes.json");
+const ingredients_recipes = require("../data/ingredients-recipes.json");
 
 const dbSeeding = async () => {
   try {
     await Ingredient.bulkCreate(ingredients);
     await Recipe.bulkCreate(recipes);
+    await db.models.ingredients_recipes.bulkCreate(ingredients_recipes);
   } catch (error) {
     console.log(error);
   }
