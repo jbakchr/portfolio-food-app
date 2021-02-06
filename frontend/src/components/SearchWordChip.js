47a90ee6cf81f73fe7291080f7a2d08495428c1c
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-06 13:04:23 +0100

    Added margin between search word chips

--- frontend/src/components/SearchWordChip.js
+++ frontend/src/components/SearchWordChip.js
@@ -4,6 +4,7 @@ import { Chip } from "@material-ui/core";
 const SearchWordChip = ({ label }) => {
   return (
     <Chip
+      style={{ marginRight: 5 }}
       label={label}
       color="primary"
       onDelete={() => console.log("Delete me!")}
