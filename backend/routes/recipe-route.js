3796c2bcb970842f5e2213395152b5841037b256
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-08 08:55:37 +0100

    Added initial fetch of recipes upon search word click

--- backend/routes/recipe-route.js
+++ backend/routes/recipe-route.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json("You made it!");
+  res.json([]);
 });
 
 module.exports = router;
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Container } from "@material-ui/core";
+import axios from "axios";
 
 import Navbar from "../components/Navbar";
 import SearchField from "../components/SearchField";
@@ -12,6 +12,7 @@ const LandingPage = () => {
   const [searchWords, setSearchWords] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [searchWordSelections, setSearchWordSelections] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     fetchSearchWords();
@@ -32,16 +33,32 @@ const LandingPage = () => {
     setSearchText(text);
   };
 
+  // TODO: fix error handling
   const searchWordClickHandler = async (index) => {
-    // When a search word is clicked we first get a copy of the search words
-    // from which we extract the clicked search word
+    // When a search word is clicked we first create a copy of the present
+    // search words and from here extract the clicked search word
     let searchWordsCopy = [...searchWords];
     const clickedSearchWord = searchWordsCopy.splice(index, 1);
 
-    // Set state
+    // Then we fetch recipes based on the users selection
+    let recipes = await fetchRecipes(clickedSearchWord);
+    console.log(recipes);
+
+    // And finally we set state
     setSearchWords(searchWordsCopy);
-    setSearchText("");
     setSearchWordSelections(searchWordSelections.concat(clickedSearchWord));
+    setSearchText("");
+    setRecipes(recipes);
+  };
+
+  const fetchRecipes = async (clickedSearchWord) => {
+    let recipes;
+    try {
+      recipes = await axios.get("http://localhost:5000/api/recipes");
+      return recipes.data;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const conditionalListRendering = () => {
