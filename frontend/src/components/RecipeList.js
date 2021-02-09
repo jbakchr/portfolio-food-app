7c9c1b4aacf3d93a74f20d5cac83709741e5e933
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-09 01:39:06 +0100

    Removed some logs not needed anymore

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -3,11 +3,8 @@ import React from "react";
 import RecipeCard from "./RecipeCard";
 
 const RecipeList = ({ recipes }) => {
-  console.log("recipes", recipes);
-
   const renderRecipeCards = () => {
     return recipes.map((recipe) => {
-      console.log(recipe);
       return <RecipeCard key={recipe.recipe} recipe={recipe} />;
     });
   };
