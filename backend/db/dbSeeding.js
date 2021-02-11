1065c9098402a9bfa83cb6241b08799e03b8eb5c
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-11 10:43:41 +0100

    Added initial food type model

--- backend/db/dbSeeding.js
+++ backend/db/dbSeeding.js
@@ -1,17 +1,29 @@
 const db = require("../db/db");
 
+// Models
 const Ingredient = require("../models/Ingredient");
+const FoodType = require("../models/FoodType");
 const Recipe = require("../models/Recipe");
 
-const ingredients = require("../data/ingredients.json");
+// Data
 const recipes = require("../data/recipes.json");
+
+const ingredients = require("../data/ingredients.json");
 const ingredients_recipes = require("../data/ingredients-recipes.json");
+
+const foodtypes = require("../data/foodtypes.json");
+const foodtypes_recipes = require("../data/foodtypes-recipes.json");
 
 const dbSeeding = async () => {
   try {
+    // Ingredients - Recipes
     await Ingredient.bulkCreate(ingredients);
     await Recipe.bulkCreate(recipes);
     await db.models.ingredients_recipes.bulkCreate(ingredients_recipes);
+
+    // FoodTypes - Recipes
+    await FoodType.bulkCreate(foodtypes);
+    await db.models.foodtypes_recipes.bulkCreate(foodtypes_recipes);
   } catch (error) {
     console.log(error);
   }
---
+++ backend/models/FoodType.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+
+const db = require("../db/db");
+const Recipe = require("../models/Recipe");
+
+const FoodType = db.define(
+  "foodtype",
+  {
+    foodtype: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+);
+
+FoodType.belongsToMany(Recipe, {
+  through: "foodtypes_recipes",
+  timestamps: false,
+});
+
+module.exports = FoodType;
