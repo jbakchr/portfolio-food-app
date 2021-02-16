81e88abbf0d43dbe927f077d5004168f502a0c3d
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-16 19:42:21 +0100

    Added some super awesome styling to the search field ..

--- frontend/src/components/SearchField.js
+++ frontend/src/components/SearchField.js
@@ -3,12 +3,26 @@ import { TextField } from "@material-ui/core";
 
 const SearchField = ({ onSearchTextChange, searchText }) => {
   return (
-    <TextField
-      variant="filled"
-      fullWidth
-      onChange={(e) => onSearchTextChange(e.target.value)}
-      value={searchText}
-    />
+    <div
+      style={{
+        backgroundColor: "lightgray",
+        borderRadius: "25px",
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+      }}
+    >
+      <TextField
+        fullWidth
+        autoFocus
+        placeholder="Søg på ingredienser, måtid osv."
+        type="search"
+        InputProps={{ disableUnderline: true }}
+        onChange={(e) => onSearchTextChange(e.target.value)}
+        value={searchText}
+      />
+    </div>
   );
 };
 
