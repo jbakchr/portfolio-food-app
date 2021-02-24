b81f4771d9517f851b21bf452202e984084afddd
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-24 22:11:41 +0100

    Added body-parser

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 // Routes
 const searchWordRoute = require("./routes/search-word-route");
 const recipeRoute = require("./routes/recipe-route");
+const authRoute = require("./routes/auth-route");
 
 // DB
 const db = require("./db/db");
@@ -20,6 +21,7 @@ app.use(cors());
 // Middleware routes
 app.use("/api/search-words", searchWordRoute);
 app.use("/api/recipes", recipeRoute);
+app.use("/api/auth", authRoute);
 
 db.sync({ force: true })
   .then(() => {
