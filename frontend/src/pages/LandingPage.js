b75939d60f7e76e350c4f044cc465d228a1f9734
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-02-24 16:58:27 +0100

    Removed unneeded import of navbar in LandingPage

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "@material-ui/core";
 
-import Navbar from "../components/Navbar";
 import SearchField from "../components/SearchField";
 import SearchWordChipList from "../components/SearchWordChipList";
 import SearchWordList from "../components/SearchWordList";
